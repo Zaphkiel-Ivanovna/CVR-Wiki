41455368a53456bb6d69fa4a735c3e689c651145
Zaphkiel Ivanovna Serebryakov <duambi123@gmail.com>
2022-07-28 13:11:25 +0200

    First Upload

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ChilloutVR CCK FR',
+  tagline: "Tutoriels sur l'utilisation du CCK en Français",
+  url: 'https://cvr-tutos.zaphkiel.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Alpha Blend Interactive', // Usually your GitHub org/user name.
+  projectName: 'cvr-tutos', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        gtag: {
+          trackingID: 'G-318RQSTQLP',
+        },
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        }
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      navbar: {
+        logo: {
+          alt: 'CVR Tutoriels FR',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutoriels',
+          },
+          {
+            to: 'https://discord.gg/4uxcxKU2pE',
+            label: 'Discord CVR FR',
+            position: 'right',
+          },
+          {
+            to: 'https://discord.gg/abi',
+            label: 'Discord ABI',
+            position: 'right',
+          }
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
